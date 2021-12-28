d7bd32288ca497ca6ae302bc1deb8c0220f7b7bf
Rhys Garman <rhys.garman@virginmedia.com>
2021-12-28 22:55:52 +0000

    Added dry run of compiler and working assembly lines

--- src/mainStack.java
+++ src/mainStack.java
@@ -4,15 +4,23 @@ import java.util.List;
 
 public class mainStack {
     
+    // List<List<String>> lexStack = new ArrayList<List<String>>();
     List<String> lexStack = new ArrayList<String>();
 
     public mainStack(String rawSource){
+        rawSource = rawSource.replace(" ", "");
+
         //Lexical analysis
         lexStack = Arrays.asList(rawSource.split(";"));
     }
 
 
     public String toString(){
+        System.out.println("\n");
+
+        for (String string : lexStack) {
+            System.out.println(string + "\n");
+        }
         return lexStack.toString();
     }
 }

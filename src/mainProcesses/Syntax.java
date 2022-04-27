452221f3407d418a2228e567483eaf83ba7569ac
Rhys Garman <rhys.garman@virginmedia.com>
2022-04-27 16:59:16 +0100

    update

--- src/mainProcesses/Syntax.java
+++ src/mainProcesses/Syntax.java
@@ -6,27 +6,24 @@ import Structures.*;
 import Structures.Functions.Function;
 
 public class Syntax {
-	
-	public Syntax(Tokenise codeBase){
-		ArrayList<Struc> code = codeBase.getFullCode();
-		
-		for (Struc structure : code) {
 
-			if(structure.getClass() == Variable.class){ // Found a variable
-				
-			}else if(structure.getClass() == Function.class){ // Found a function (look inside)
-				Function crnt = (Function) structure;
-				// for (Struc struc : ) {
-					
-				// }
+	public Syntax(Tokenise codeBase) {
+		ArrayList < Struc > code = codeBase.getFullCode();
+
+		for (Struc structure: code) {
+
+			if (structure.getClass() == Variable.class) { // Found a variable
+
+			} else if (structure.getClass() == Function.class) { // Found a function (look inside)
+
 			}
 		}
-		
-		
-		
-			
-        
-        return;
-        
+
+
+
+
+
+		return;
+
 	}
 }

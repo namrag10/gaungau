c69b920db610eebdcf95bb8bda437dddb9f82c32
Rhys Garman <rhys.garman@virginmedia.com>
2022-06-14 13:49:51 +0100

    update

--- src/mainProcesses/output.java
+++ src/mainProcesses/output.java
@@ -1,6 +1,7 @@
 package mainProcesses;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Queue;
 
 import Structures.Meta.LineMeta;
@@ -9,9 +10,10 @@ import Structures.Meta.Struc;
 public class output {
 
     public ArrayList<Struc> stack = new ArrayList<Struc>();
+    public Queue<LineMeta> lex = new LinkedList<LineMeta>();
 
     public output(Queue<LineMeta> lexStack){
-
+        lex = lexStack;
     }
 
     public output(ArrayList<Struc> synStack){
